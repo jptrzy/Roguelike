7e6d24a0f995c5a8051558e90451ecca4bad1f8d
szszs <shawn.zhou2@gmail.com>
2018-05-24 00:05:25 +0800

    Started work on items

--- items.py
+++ items.py
@@ -4,13 +4,3 @@ class item(object):
 		self.name = name
 		self.weight = weight
 		self.desc = desc
-
-class weapon(item):
-	def __init__(self, name, weight, desc):
-		item.__init__(self, name, weight, desc)
-
-class consumable(item):
-	def __init__(self, name, weight, desc):
-		item.__init__(self, name, weight, desc)
-
-axe = weapon('axe', 10)
